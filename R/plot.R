117b428134b562c43e1962e65a7426266c65b66b
Christian Amesöder <christian.amesoeder@stud.uni-regensburg.de>
2022-04-01 13:32:49 +0200

    bugfix + example in analyze_training()

--- R/plot.R
+++ R/plot.R
@@ -24,7 +24,25 @@ visualize.training <- function(losses,epoch){
 #'
 #' @param object a model created by \code{\link{dnn}}
 #' @return a plotly figure
+#' @examples
+#' library(cito)
+#' set.seed(222)
+#' validation_set<- sample(c(1:nrow(datasets::iris)),25)
 #'
+#' # Build and train  Network
+#' nn.fit<- dnn(Sepal.Length~., data = datasets::iris[-validation_set,],validation = 0.1)
+#'
+#' # show zoomable plot of training and validation losses
+#' analyze_training(nn.fit)
+#'
+#' # set model which is used for predictions to model from epoch 46
+#' nn.fit$use_model_epoch <- 46
+#'
+#' #' # Use model on validation set
+#' predictions <- predict(nn.fit, iris[validation_set,])
+#'
+#' # Scatterplot
+#' plot(iris[validation_set,]$Sepal.Length,predictions)
 #' @export
 
 analyze_training<- function(object){
@@ -33,7 +51,7 @@ analyze_training<- function(object){
                          width = 900)
 
   fig<- plotly::add_trace(fig,x = ~epoch, y = ~train_l,text = "Training Loss")
-  if(object$call$validation>0 && !is.null(object$call$activation))  {
+  if(object$call$validation>0 && !is.null(object$call$validation))  {
     fig<- plotly::add_trace(fig,x = ~epoch, y = ~valid_l, text ="Validation loss")
   }
   fig<- plotly::layout(fig, showlegend = F, title='DNN Training',
