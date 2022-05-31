ef3cb3d54191b095eefd9f22cbf67453720e830a
Christian Amesöder <christian.amesoeder@stud.uni-regensburg.de>
2022-05-31 12:22:18 +0200

    exchanged startupmessage function

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
 
 .onLoad = function(libname, pkgname){
 if(!torch::torch_is_installed()){
-  message("Torch is not yet installed")
-  message("Please run the following before using cito")
-  message("\"library(\"torch\")\"")
-  message("\"install_torch()\"")
+  packageStartupMessage("Torch is not yet installed")
+  packageStartupMessage("Please run the following before using cito")
+  packageStartupMessage("\"library(\"torch\")\"")
+  packageStartupMessage("\"install_torch()\"")
+  packageStartupMessage("see: https://torch.mlverse.org/docs/articles/installation.html")
   }
 }
