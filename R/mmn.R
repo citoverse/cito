d964cfc76dfae128457eb6d8c364a7b0b4dbeba1
Armin Schenk <armin.schenk99@gmail.com>
2024-10-25 12:47:01 +0200

    Update mmn documentation and implement the check_mmn_formula function

--- R/mmn.R
+++ R/mmn.R
@@ -1,6 +1,6 @@
-#' Train and evaluate a Multi-Modal Neural Network (MMN) model
+#' Train a Multi-Modal Neural Network (MMN)
 #'
-#' This function trains a Multi-Modal Neural Network (MMN) model on the provided data.
+#' This function trains a Multi-Modal Neural Network (MMN) which consists of a combination of DNNs and CNNs.
 #'
 #' @param formula A formula object specifying the model structure. See examples for more information
 #' @param dataList A list containing the data for training the model. The list should contain all variables used in the formula.
@@ -8,45 +8,56 @@
 #' @param fusion_activation A character vector specifying the activation function for each hidden layer of the fusion network. Available options are: "relu", "leaky_relu", "tanh", "elu", "rrelu", "prelu", "softplus", "celu", "selu", "gelu", "relu6", "sigmoid", "softsign", "hardtanh", "tanhshrink", "softshrink", "hardshrink", "log_sigmoid".
 #' @param fusion_bias A logical value or vector (length(fusion_hidden) + 1) indicating whether to include bias terms in the layers of the fusion network.
 #' @param fusion_dropout The dropout rate for the fusion network, a numeric value or vector (length(fusion_hidden)) between 0 and 1.
-#' @param loss The loss function to be optimized during training. Available options are: "mse", "mae", "softmax", "cross-entropy", "gaussian", "binomial", "poisson".
-#' @param optimizer The optimization algorithm to be used during training. Available options are: "sgd", "adam", "adadelta", "adagrad", "rmsprop", "rprop".
-#' @param lr The learning rate for the optimizer.
-#' @param alpha The alpha parameter for elastic net regularization. Should be a value between 0 and 1.
-#' @param lambda The lambda parameter for elastic net regularization. Should be a positive value.
-#' @param validation The proportion of the training data to use for validation. Should be a value between 0 and 1.
-#' @param batchsize The batch size used during training.
-#' @param burnin training is aborted if the trainings loss is not below the baseline loss after burnin epochs
-#' @param shuffle A logical indicating whether to shuffle the training data in each epoch.
-#' @param epochs The number of epochs to train the model.
-#' @param early_stopping If provided, the training will stop if the validation loss does not improve for the specified number of epochs. If set to NULL, early stopping is disabled.
-#' @param lr_scheduler Learning rate scheduler created with \code{\link{config_lr_scheduler}}
-#' @param custom_parameters A list of parameters used by custom loss functions. See vignette for examples.
-#' @param device The device on which to perform computations. Available options are: "cpu", "cuda", "mps".
-#' @param plot A logical indicating whether to plot training and validation loss curves.
-#' @param verbose A logical indicating whether to display verbose output during training.
+#' @param loss The loss function to be used. Options include "mse", "mae", "softmax", "cross-entropy", "gaussian", "binomial", "poisson", "nbinom", "mvp", "multinomial", and "clogit". You can also specify your own loss function. See Details for more information. Default is "mse".
+#' @param optimizer The optimizer to be used. Options include "sgd", "adam", "adadelta", "adagrad", "rmsprop", and "rprop". See \code{\link{config_optimizer}} for further adjustments to the optimizer. Default is "sgd".
+#' @param lr Learning rate for the optimizer. Default is 0.01.
+#' @param alpha Alpha value for L1/L2 regularization. Default is 0.5.
+#' @param lambda Lambda value for L1/L2 regularization. Default is 0.0.
+#' @param validation Proportion of the data to be used for validation. Default is 0.0.
+#' @param batchsize Batch size for training. Default is 32.
+#' @param burnin Number of epochs after which the training stops if the loss is still above the base loss. Default is Inf.
+#' @param shuffle Whether to shuffle the data before each epoch. Default is TRUE.
+#' @param epochs Number of epochs to train the model. Default is 100.
+#' @param early_stopping Number of epochs with no improvement after which training will be stopped. Default is NULL.
+#' @param lr_scheduler Learning rate scheduler. See \code{\link{config_lr_scheduler}} for creating a learning rate scheduler. Default is NULL.
+#' @param custom_parameters Parameters for the custom loss function. See the vignette for an example. Default is NULL.
+#' @param device Device to be used for training. Options are "cpu", "cuda", and "mps". Default is "cpu".
+#' @param plot Whether to plot the training progress. Default is TRUE.
+#' @param verbose Whether to print detailed training progress. Default is TRUE.
 #'
-#' @return An object of class "citommn" containing the trained MMN model and other information.
-#' @export
-#'
+#' @return An S3 object of class \code{"citommn"} is returned. It is a list containing everything there is to know about the model and its training process.
+#' The list consists of the following attributes:
+#' \item{net}{An object of class "nn_module", originates from the torch package and represents the core object of this workflow.}
+#' \item{call}{The original function call.}
+#' \item{loss}{A list which contains relevant information for the target variable and the used loss function.}
+#' \item{data}{Contains the data used for the training of the model.}
+#' \item{base_loss}{The loss of the intercept-only model.}
+#' \item{weights}{List of parameters (weights and biases) of the models from the best and the last training epoch.}
+#' \item{buffers}{List of buffers (e.g. running mean and variance of batch normalization layers) of the models from the best and the last training epoch.}
+#' \item{use_model_epoch}{Integer, defines whether the model from the best (= 1) or the last (= 2) training epoch should be used for prediction.}
+#' \item{loaded_model_epoch}{Integer, shows whether the parameters and buffers of the model from the best (= 1) or the last (= 2) training epoch are currently loaded in \code{net}.}
+#' \item{model_properties}{A list of properties, that define the architecture of the model.}
+#' \item{training_properties}{A list of all the training parameters used the last time the model was trained.}
+#' \item{losses}{A data.frame containing training and validation losses of each epoch.}
+#' @import checkmate
+#' @example /inst/examples/mmn-example.R
+#' @author Armin Schenk
 #' @seealso \code{\link{predict.citommn}}, \code{\link{print.citommn}}, \code{\link{summary.citommn}}, \code{\link{continue_training}}, \code{\link{analyze_training}}
-#'
 #' @export
 mmn <- function(formula,
                 dataList = NULL,
                 fusion_hidden = c(50L, 50L),
-                fusion_activation = c("relu", "leaky_relu", "tanh", "elu", "rrelu", "prelu", "softplus",
-                               "celu", "selu", "gelu", "relu6", "sigmoid", "softsign", "hardtanh",
-                               "tanhshrink", "softshrink", "hardshrink", "log_sigmoid"),
+                fusion_activation = "selu",
                 fusion_bias = TRUE,
                 fusion_dropout = 0.0,
-                loss = c("mse", "mae", "softmax", "cross-entropy", "gaussian", "binomial", "poisson"),
+                loss = c("mse", "mae", "softmax", "cross-entropy", "gaussian", "binomial", "poisson", "mvp", "nbinom", "multinomial", "clogit"),
                 optimizer = c("sgd", "adam", "adadelta", "adagrad", "rmsprop", "rprop"),
                 lr = 0.01,
                 alpha = 0.5,
                 lambda = 0.0,
                 validation = 0.0,
                 batchsize = 32L,
-                burnin = 10,
+                burnin = Inf,
                 shuffle = TRUE,
                 epochs = 100,
                 early_stopping = NULL,
@@ -74,21 +85,28 @@ mmn <- function(formula,
   checkmate::qassert(device, "S+[3,)")
 
   device <- match.arg(device)
-  device_old <- device
-  device <- check_device(device)
-
-  if(identical (fusion_activation, c("relu", "leaky_relu", "tanh", "elu", "rrelu", "prelu", "softplus",
-                              "celu", "selu", "gelu", "relu6", "sigmoid", "softsign", "hardtanh",
-                              "tanhshrink", "softshrink", "hardshrink", "log_sigmoid"))) fusion_activation<- "relu"
-
-
 
   if(!is.function(loss) & !inherits(loss,"family")) {
     loss <- match.arg(loss)
+
+    if((device == "mps") & (loss %in% c("poisson", "nbinom", "multinomial"))) {
+      message("`poisson`, `nbinom`, and `multinomial` are not yet supported for `device=mps`, switching to `device=cpu`")
+      device = "cpu"
+    }
   }
 
-  loss_obj <- get_loss(loss)
-  if(!is.null(loss_obj$parameter)) loss_obj$parameter <- list(scale = loss_obj$parameter)
+  if(inherits(loss,"family")) {
+    if((device == "mps") & (loss$family %in% c("poisson", "nbinom"))) {
+      message("`poisson` or `nbinom` are not yet supported for `device=mps`, switching to `device=cpu`")
+      device = "cpu"
+    }
+  }
+
+  device_old <- device
+  device <- check_device(device)
+
+  loss_obj <- get_loss(loss, device = device)
+  if(!is.null(loss_obj$parameter)) loss_obj$parameter <- list(parameter = loss_obj$parameter)
   if(!is.null(custom_parameters)){
     if(!inherits(custom_parameters,"list")){
       warning("custom_parameters has to be list")
@@ -99,8 +117,7 @@ mmn <- function(formula,
   }
 
   formula <- stats::terms(formula)
-  if(formula[[1]] != "~") stop(paste0("Incorrect formula '", deparse(formula), "': ~ missing"))
-  if(length(formula) == 2) stop("Incorrect formula '", deparse(formula), "': response (left side of ~) missing")
+  check_mmn_formula(formula)
 
   Y <- eval(formula[[2]], envir = dataList)
   X <- format_input_data(formula[[3]], dataList)
@@ -114,7 +131,7 @@ mmn <- function(formula,
 
   loss.fkt <- loss_obj$loss
   if(!is.null(loss_obj$parameter)) list2env(loss_obj$parameter,envir = environment(fun= loss.fkt))
-  base_loss = as.numeric(loss.fkt(loss_obj$link(Y_base), Y_torch)$mean())
+  base_loss = as.numeric(loss.fkt(torch::torch_tensor(loss_obj$link(Y_base$cpu()), dtype = Y_base$dtype)$to(device = device), Y_torch$to(device = device))$mean()$cpu())
 
   if(validation != 0) {
     n_samples <- dim(Y_torch)[1]
@@ -285,16 +302,11 @@ format_input_data <- function(formula, dataList) {
           X <- tmp[, -1, drop=FALSE]
           attr(X, "assign") <- attr(tmp, "assign")[-1]
         }
-      } else {
-        stop(paste0("In '", deparse(term), "' either 'formula' or 'X' must be specified."))
       }
-
       input_data <- append(input_data, list(X))
     } else if(term[[1]] == "cnn") {
       call <- match.call(cnn, term)
       input_data <- append(input_data, list(eval(call$X, envir = dataList)))
-    } else {
-      stop(paste0("Symbol not supported: ", term[[1]]))
     }
     return(input_data)
   }
@@ -314,13 +326,50 @@ get_model_properties <- function(formula, dataList) {
     } else if(term[[1]] == "cnn") {
       call <- match.call(cnn, term)
       model_properties <- append(model_properties, list(eval(call, envir = dataList)))
-    } else {
-      stop(paste0("Symbol not supported: ", term[[1]]))
     }
     return(model_properties)
   }
 
   return(inner(formula, list()))
+}
+
+check_mmn_formula <- function(formula) {
+
+  inner <- function(term) {
+    if(term[[1]] == "+") {
+      inner(term[[2]])
+      inner(term[[3]])
+    } else if(term[[1]] == "dnn") {
+      call <- match.call(dnn, term)
+      if(is.null(call$formula) & is.null(call$X)) stop(paste0("In '", deparse(term), "': Either 'formula' or 'X' must be specified."))
+      if(!is.null(call$Y)) stop(paste0("In '", deparse(term), "': 'Y' must not be specified."))
+      if(!is.null(call$formula)) {
+        if(call$formula[[1]] != "~") stop(paste0("In '", deparse(term), "': ~ missing in 'formula'."))
+        if(length(call$formula) != 2) stop(paste0("In '", deparse(term), "': response (left side of ~) of 'formula' must not be specified."))
+      }
+      args <- names(formals(dnn))
+      allowed_args <- c("formula", "data", "hidden", "activation", "bias", "dropout", "X")
+      for(arg in setdiff(args, allowed_args)) {
+        if(!is.null(call[[arg]])) warning(paste0("In '", deparse(term), "': Specifying '", arg, "' here has no effect. When using dnn() within mmn() only the following arguments are important: ", paste0(allowed_args, collapse = ", ")))
+      }
+    } else if(term[[1]] == "cnn") {
+      call <- match.call(cnn, term)
+      if(!is.null(call$Y)) stop(paste0("In '", deparse(term), "': 'Y' must not be specified."))
+      if(is.null(call$X)) stop(paste0("In '", deparse(term), "': 'X' must be specified."))
+      if(is.null(call$architecture)) stop(paste0("In '", deparse(term), "': 'architecture' must be specified."))
+      args <- names(formals(cnn))
+      allowed_args <- c("X", "architecture")
+      for(arg in setdiff(args, allowed_args)) {
+        if(!is.null(call[[arg]])) warning(paste0("In '", deparse(term), "': Specifying '", arg, "' here has no effect. When using cnn() within mmn() only the following arguments are important: ", paste0(allowed_args, collapse = ", ")))
+      }
+    } else {
+      stop(paste0("Symbol not supported: ", term[[1]]))
+    }
+  }
+
+  if(formula[[1]] != "~") stop(paste0("Incorrect formula '", deparse(formula), "': ~ missing."))
+  if(length(formula) == 2) stop(paste0("Incorrect formula '", deparse(formula), "': response (left side of ~) missing."))
+  inner(formula[[3]])
 }
 
 
