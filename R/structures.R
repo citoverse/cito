46ee5c209e1241d7daa8262fdaf3904c841c36c7
Florian Hartig <florianhartig@users.noreply.github.com>
2025-01-15 10:31:21 +0100

    Help update for embeddings

--- R/structures.R
+++ R/structures.R
@@ -1,6 +1,6 @@
 #' Embeddings
 #'
-#' Can be used for categorical variables, a more efficient alternative to one-hot encoding
+#' Can be used to create an embedding structure for categorical variables in the function interface
 #'
 #' @param dim integer, embedding dimension
 #' @param weights matrix, to use custom embedding matrices
@@ -8,6 +8,14 @@
 #' @param lambda regularization strength on the embeddings
 #' @param alpha mix between L1 and L2 regularization
 #'
+#' @details The e() structure must be used in the function interface. Although not document in the function signature, the first argument to the e() structure is the categorical variable that codes a group in the data, as in
+#'
+#' predictors + e(group, ...)
+#'
+#' For more details, see the example below
+#'
+#'
+#' @example /inst/examples/e-example.R
 #' @export
 
 e = function(dim = 1L, weights = NULL, train = TRUE, lambda = 0.0, alpha = 1.0) {
