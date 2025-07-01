de533c6908a4e59ec1a6b60664b6ab9a38c0deb8
Armin Schenk <armin.schenk99@gmail.com>
2025-07-01 19:31:07 +0200

    Move testing of loss functions to separate file

--- R/utils.R
+++ R/utils.R
@@ -659,6 +659,8 @@ get_loss_new <- function(loss, Y, custom_parameters) {
         return(sim)
       }
     )
+  } else {
+    stop("Loss not implemented.")
   }
 
   return(create_loss())
--- tests/testthat/test-mmn.R
+++ tests/testthat/test-mmn.R
@@ -2,160 +2,31 @@ source("utils.R")
 library(cito)
 set.seed(42)
 
-wrap_mmn = function(pars) {
-  testthat::expect_error({model = do.call(mmn, pars)}, NA)
-  testthat::expect_error({.n = predict(model)}, NA)
-  testthat::expect_error({.n = continue_training(model, epochs = 1L, verbose = FALSE)}, NA)
-  testthat::expect_error({.n = predict(model)}, NA)
-  testthat::expect_error({.n = predict(model, type = "response")}, NA)
-  testthat::expect_error({.n = coef(model)}, NA)
-  testthat::expect_error({.n = residuals(model)}, NA)
-  testthat::expect_error({.n = summary(model)}, NA)
-}
 
-array_dims <- list(c(100,1,10),
-                   c(100,1,10,10),
-                   c(100,1,10,10,10),
-                   c(100,3,10),
-                   c(100,3,10,10),
-                   c(100,3,10,10,10))
-X_cnn <- lapply(array_dims, function(x) array(runif(prod(x)), dim = x))
-X_dnn <- matrix(runif(300), 100, 3)
 
-architecture <- create_architecture(conv(), maxPool(), conv(), avgPool(), linear())
+testthat::test_that("MMN architecture", {
+  testthat::skip_on_cran()
+  testthat::skip_on_ci()
+  skip_if_no_torch()
 
-scenarios <- lapply(X_cnn, function(x) {
-  list(formula=stats::as.formula("Y~cnn(X=X1, architecture=architecture) + dnn(X=X2)"), dataList=list(X1=x, X2=X_dnn), epochs=1, plot=FALSE, verbose=FALSE)
-})
-
-test_loss <- function(loss, Y) {
-  for(i in 1:length(scenarios)) {
-    sc <- scenarios[[i]]
-    sc <- append(sc, list(loss=loss))
-    for(j in 1:length(Y)) {
-      sc$dataList$Y <- Y[[j]]
-
-      .n <- wrap_mmn(append(sc, list(device="cpu")))
-      if(torch::cuda_is_available()) {
-        .n <- wrap_mmn(append(sc, list(device="cuda")))
-      }
-      if(torch::backends_mps_is_available()) {
-        .n <- wrap_mmn(append(sc, list(device="mps")))
-      }
-    }
+  if(torch::cuda_is_available()) {
+    device <- "cuda"
+  } else if(torch::backends_mps_is_available()) {
+    device <- "mps"
+  } else {
+    device <- "cpu"
   }
-}
+
+  set.seed(42)
+  architecture1 <- create_architecture(conv(), maxPool(), conv(), avgPool(), linear())
+  architecture2 <- create_architecture(conv(), conv(), maxPool(), conv(), conv(), avgPool(), linear(), linear())
+  X_cnn1 <- array(runif(prod(c(100,3,10,10))), dim = c(100,3,10,10))
+  X_cnn2 <- array(runif(prod(c(100,1,50,50))), dim = c(100,1,50,50))
+  X_dnn1 <- matrix(runif(100*3), 100, 3)
+  X_dnn2 <- factor(sample(c("a","b","c"), 100, replace=TRUE))
+  Y <- factor(sample(c("d","e","f"), 100, replace=TRUE))
 
 
-testthat::test_that("MMN cross-entropy", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
+  .n <- wrap_mmn(list(formula=Y ~ cnn(X=X_cnn1, architecture=architecture1) + dnn(X=X_dnn1, hidden=c(30,30,30)) + cnn(X=X_cnn2, architecture=architecture2) + dnn(~X_dnn2), dataList=list(Y=y, X_cnn1=X_cnn1, X_cnn2=X_cnn2, X_dnn1=X_dnn1, X_dnn2=X_dnn2), loss="cross-entropy", device=device, epochs=1, plot=FALSE, verbose=FALSE))
 
-  Y <- list(factor(sample(c("a","b","c"), 100, replace=TRUE)),
-            sample(c("a","b","c"), 100, replace=TRUE),
-            matrix(sample(c("a","b","c"), 100, replace=TRUE), nrow=100, ncol=1),
-            data.frame(sample(c("a","b","c"), 100, replace=TRUE)))
-
-  test_loss("cross-entropy", Y)
-})
-
-testthat::test_that("MMN mse/mae/gaussian", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(runif(100),
-            matrix(runif(100), nrow=100, ncol=1),
-            matrix(runif(300), nrow=100, ncol=3),
-            data.frame(runif(100)),
-            data.frame(runif(100), runif(100), runif(100)))
-
-  test_loss("mse", Y)
-  test_loss("mae", Y)
-  test_loss("gaussian", Y)
-})
-
-testthat::test_that("MMN poisson", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(rpois(100, 10),
-            matrix(rpois(100, 10), nrow=100, ncol=1),
-            matrix(rpois(300, 10), nrow=100, ncol=3),
-            data.frame(rpois(100, 10)),
-            data.frame(rpois(100, 10), rpois(100, 20), rpois(100, 30)))
-
-  test_loss("poisson", Y)
-})
-
-testthat::test_that("MMN nbinom", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(rnbinom(100, size=0.5, mu=10),
-            matrix(rnbinom(100, size=0.5, mu=10), nrow=100, ncol=1),
-            matrix(rnbinom(300, size=0.5, mu=10), nrow=100, ncol=3),
-            data.frame(rnbinom(300, size=0.5, mu=10)),
-            data.frame(rnbinom(300, size=0.5, mu=10), rnbinom(300, size=0.5, mu=20), rnbinom(300, size=0.5, mu=30)))
-
-  test_loss("nbinom", Y)
-})
-
-testthat::test_that("MMN binomial", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(factor(sample(c("a","b"), 100, replace=TRUE)),
-            sample(c("a","b"), 100, replace=TRUE),
-            matrix(sample(c("a","b"), 100, replace=TRUE), nrow=100, ncol=1),
-            data.frame(sample(c("a","b"), 100, replace=TRUE)),
-            matrix(sample(0:10, 200, replace = TRUE), nrow=100, ncol=2),
-            data.frame(sample(0:10, 100, replace = TRUE), sample(0:10, 100, replace = TRUE)))
-
-  test_loss("binomial", Y)
-})
-
-testthat::test_that("MMN multinomial", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(factor(sample(c("a","b","c"), 100, replace=TRUE)),
-            sample(c("a","b","c"), 100, replace=TRUE),
-            matrix(sample(c("a","b","c"), 100, replace=TRUE), nrow=100, ncol=1),
-            data.frame(sample(c("a","b","c"), 100, replace=TRUE)),
-            matrix(sample(0:10, 300, replace = TRUE), nrow=100, ncol=3),
-            data.frame(sample(0:10, 100, replace = TRUE), sample(0:10, 100, replace = TRUE), sample(0:10, 100, replace = TRUE)))
-
-  test_loss("multinomial", Y)
-})
-
-testthat::test_that("MMN mvp", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(matrix(sample(c(0,1), 300, replace = TRUE), nrow=100, ncol=3),
-            data.frame(sample(c(0,1), 100, replace = TRUE), sample(c(0,1), 100, replace = TRUE), sample(c(0,1), 100, replace = TRUE)))
-
-  test_loss("mvp", Y)
-})
-
-testthat::test_that("MMN clogit", {
-  testthat::skip_on_cran()
-  testthat::skip_on_ci()
-  skip_if_no_torch()
-
-  Y <- list(factor(sample(c("a","b","c"), 100, replace=TRUE)),
-            sample(c("a","b","c"), 100, replace=TRUE),
-            matrix(sample(c("a","b","c"), 100, replace=TRUE), nrow=100, ncol=1),
-            data.frame(sample(c("a","b","c"), 100, replace=TRUE)),
-            matrix(sample(c(0,1), 300, replace = TRUE), nrow=100, ncol=3),
-            data.frame(sample(c(0,1), 100, replace = TRUE), sample(c(0,1), 100, replace = TRUE), sample(c(0,1), 100, replace = TRUE)))
-
-  test_loss("clogit", Y)
 })
